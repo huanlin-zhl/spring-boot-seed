777249e4b37f70670cf55817a67425d96322caf8
huanlin-zhl <zhanghhll.163@qq.com>
2020-03-31 16:34:35 +0800

    initialize commit

---
+++ src/main/java/com/zhl/springbootseed/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.zhl.springbootseed.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+}
---
+++ src/test/java/com/zhl/springbootseed/SpringBootSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhl.springbootseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
